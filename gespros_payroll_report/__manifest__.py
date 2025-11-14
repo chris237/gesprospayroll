4c0add834ede73c0e8a1a9175f77c5625319b40e
Christian Ferdinand FOTIE <31020827+chris237@users.noreply.github.com>
2025-11-14 17:48:49 +0100

    Inline payslip styles and clean legacy view

---
+++ gespros_payroll_report/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "GESPROS Payroll Payslip",
+    "version": "18.0.1.0.1",
+    "summary": "Custom payslip layout inspired by GESPROS template",
+    "description": """Replaces the native payroll payslip report with a branded layout.""",
+    "category": "Human Resources/Payroll",
+    "depends": ["hr_payroll"],
+    "data": [
+        "report/payslip_templates.xml"
+    ],
+    "license": "LGPL-3",
+    "author": "GESPROS",
+    "website": "https://www.gespros.com",
+    "application": False,
+    "installable": True,
+    "pre_init_hook": "pre_init_hook",
+}
